9df78e6ec5f92d347e9b1f8c34d8f342dfd49410
amanda <amanda@zipcodes-MacBook-Pro-2.local>
2020-11-11 08:27:44 -0500

    defined getExamScore() and addExamScore()

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -16,8 +16,11 @@ public class Student {
         return lastName;
     }
 
-    public ArrayList<Double> getExamScores() {
-        return examScores;
+    public ArrayList<Double> getExamScores(String firstName, String lastName, ArrayList<Double> examScores) {
+        System.out.println("Exam 1 -> " + examScores.get(0));
+        System.out.println("Exam 2 -> " + examScores.get(1));
+        System.out.println("Exam 3 -> " + examScores.get(2));
+        System.out.println("Exam 4 -> " + examScores.get(3));
     }
 
     public void setExamScores(ArrayList<Double> examScores) {
@@ -32,9 +35,13 @@ public class Student {
         this.lastName = lastName;
     }
 
-    public Integer getNumberOfExamsTaken(){
+    public Integer getNumberOfExamsTaken() {
         return examScores.size();
     }
 
-
+    public double addExamScore(double examScore) {
+        ArrayList<Double> examScores = new ArrayList<>();
+        examScores.add(examScore);
+        return examScore;
+    }
 }
